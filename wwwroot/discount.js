03a33b9e1bda371fa59e4f9ee2f1e4412dbbea91
urlockGaur <126732165+urlockGaur@users.noreply.github.com>
2024-03-14 19:18:11 -0500

    in class 4.12 github

--- wwwroot/discount.js
+++ wwwroot/discount.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('bananas');
+    document.getElementById('discount-row').addEventListener('click', function(e){
+        if (e.target.classList.contains('discount')) {
+            e.preventDefault();
+            document.getElementById('code').innerHTML = e.target.dataset['code'];
+            document.getElementById('product').innerHTML = e.target.dataset['product'];
+            document.getElementById('title').innerHTML = e.target.dataset['title'];
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+        }
+    });
 });
